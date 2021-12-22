b7c5be278d2b175faf9b3fbda05f590246654f5f
artem-kuznecov <95186298+artem-kuznecov@users.noreply.github.com>
2021-12-23 01:32:39 +0300

    Add files via upload

---
+++ lab02/main.py
@@ -0,0 +1,15 @@
+from rectangle import Rectangle
+from circle import Circle
+from square import Square
+
+
+def main():
+    r = Rectangle("синего", 3, 2)
+    c = Circle("зеленого", 5)
+    s = Square("красного", 5)
+    print(r)
+    print(c)
+    print(s)
+
+if __name__ == "__main__":
+    main()
